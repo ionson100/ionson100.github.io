b24139c5bf842b72f6c425d9664d54631da9e47c
bsr <bsr@DESKTOP-GTR4O03>
2024-07-11 15:44:38 +0200

    ++ add map

--- src/ViewStorage/menu/sidebar/7-1.tsx
+++ src/ViewStorage/menu/sidebar/7-1.tsx
@@ -1,5 +1,104 @@
 import {LeftMenu, MenuItem, CreateItem} from "bsr-left-sidebar";
-import {useEffect, useRef} from "react";
+import React, {ReactElement, useEffect, useRef} from "react";
+import {TbPointFilled} from "react-icons/tb";
+import {FaCaretDown, FaCaretRight} from "react-icons/fa";
+import {RiMenuFill} from "react-icons/ri";
+import { FcAssistant } from "react-icons/fc";
+import { FcBusinesswoman } from "react-icons/fc";
+import { FcEngineering } from "react-icons/fc";
+import { FcGallery } from "react-icons/fc";
+import {CodeSnippetJavaScript} from "../../codeSnippetJavaScript";
+
+const iconsStyle={
+    paddingLeft: "0.3rem",
+}
+function getItems():MenuItem[]{
+    const ListItem:MenuItem[]=[];
+    {
+        const m=CreateItem({content:"Item1",icon:<FcAssistant size={25} style={iconsStyle} />})
+        m.items?.push(CreateItem({content:"subItem1"}))
+        m.items?.push(CreateItem({content:"subItem2"}))
+        ListItem.push(m);
+    }
+
+    ListItem.push(CreateItem({content:"Item2",icon:<FcBusinesswoman  size={25} style={iconsStyle}/>}))
+    {
+        const m=CreateItem({content:"Item3",icon:<FcAssistant size={25} style={iconsStyle} />})
+        const sm=CreateItem({content:"subItem1"});
+        sm.items?.push(CreateItem({content:"sub_subItem1"}))
+        sm.items?.push(CreateItem({content:"sub_subItem2"}))
+        m.items?.push(sm)
+        m.items?.push(CreateItem({content:"subItem2"}))
+        ListItem.push(m);
+    }
+    ListItem.push(CreateItem({content:"Item4",icon:<FcGallery  size={25} style={iconsStyle}/>}))
+    ListItem.push(CreateItem({content:"test"}))
+    return  ListItem
+}
+const styleHeader={
+    color:"wheat",border:"3px solid wheat",padding:20
+}
+
+export default function P7_1(){
+    const mRefMenu = useRef<InstanceType<typeof LeftMenu>>(null)
+    const mRefInput = useRef<HTMLInputElement>(null)
+
+    function getHeader():ReactElement{
+        return(
+            <div style={styleHeader}>
+                <button onClick={()=>{
+                    mRefMenu.current!.Expand()
+                }}>Expand</button>
+                <button onClick={()=>{
+                    mRefMenu.current!.Collapse()
+                }}>Collapse</button>
+                <button onClick={()=>{
+                    if(!mRefInput.current?.value){
+                        alert('id for click - empty')
+                        return;
+                    }
+                    mRefMenu.current!.OpenMenuItemAndClick(mRefInput.current!.value)
+                }}>Click by Id</button>
+                <br/><br/>
+                <input ref={mRefInput} type={'text'} style={{width:270}}/>
+            </div>
+        )
+    }
+
+    return(
+        <div style={{display:"flex",width:"100%",height:"100%"}}>
+
+                <LeftMenu
+
+                    ref={mRefMenu}
+                    header={getHeader()}
+                    iconTree={<TbPointFilled size={12}/>}
+                    paddingItem={15}
+                    minWidth={55}
+                    width={350}
+                    items={getItems()}
+                    iconClose={<FaCaretRight color={"#6c6b6b"}/>}
+                    iconOpen={<FaCaretDown/>}
+                    iconToggle={<RiMenuFill size={30} color={"yellow"}/>}
+                    footer={<div style={{height: 40, borderTop: "1px solid #6c6b6b"}}></div>}
+                    onClickMenuItem={(a,o)=>{
+                       mRefInput.current!.value= o.items!.id
+                    }}
+                    onClickToggle={(state)=>{
+                        if(state){//open
+                            if(mRefInput.current!.value){
+                                mRefMenu.current!.OpenMenuItemAndClick(mRefInput.current!.value)
+                            }
+                        }
+                    }}
+
+                />
+
+
+
+            <div style={{height:"100%",width:"100%",overflowY:"auto"}}>
+                <CodeSnippetJavaScript code={`import {LeftMenu, MenuItem, CreateItem} from "bsr-left-sidebar";
+import React, {useEffect, useRef} from "react";
 import {TbPointFilled} from "react-icons/tb";
 import {FaCaretDown, FaCaretRight} from "react-icons/fa";
 import {RiMenuFill} from "react-icons/ri";
@@ -11,18 +110,63 @@ import { FcGallery } from "react-icons/fc";
 const iconsStyle={
     paddingLeft: "0.3rem",
 }
+function GetItems():MenuItem[]{
+    const ListItem:MenuItem[]=[];
+    {
+        const m=CreateItem({content:"Item1",icon:<FcAssistant size={25} style={iconsStyle} />})
+        m.items?.push(CreateItem({content:"subItem1"}))
+        m.items?.push(CreateItem({content:"subItem2"}))
+        ListItem.push(m);
+    }
+
+    ListItem.push(CreateItem({content:"Item2",icon:<FcBusinesswoman  size={25} style={iconsStyle}/>}))
+    {
+        const m=CreateItem({content:"Item3",icon:<FcAssistant size={25} style={iconsStyle} />})
+        const sm=CreateItem({content:"subItem1"});
+        sm.items?.push(CreateItem({content:"sub_subItem1"}))
+        sm.items?.push(CreateItem({content:"sub_subItem2"}))
+        m.items?.push(sm)
+        m.items?.push(CreateItem({content:"subItem2"}))
+        ListItem.push(m);
+    }
+    ListItem.push(CreateItem({content:"Item4",icon:<FcGallery  size={25} style={iconsStyle}/>}))
+    ListItem.push(CreateItem({content:"test"}))
+    return  ListItem
+}
 
 export default function P7_1(){
     const mRefMenu = useRef<InstanceType<typeof LeftMenu>>(null)
-    const ListItem:MenuItem[]=[];
-    ListItem.push(CreateItem({content:"Item1",icon:<FcAssistant size={25} style={iconsStyle}/>}));
-    ListItem.push(CreateItem({content:"Item2",icon:<FcBusinesswoman  size={25} style={iconsStyle}/>}))
-    ListItem.push(CreateItem({content:"Item3",icon:<FcEngineering  size={25} style={iconsStyle}/>}))
-    ListItem.push(CreateItem({content:"Item4",icon:<FcGallery  size={25} style={iconsStyle}/>}))
-    ListItem.push(CreateItem({content:"test"}))
+    const mRefInput = useRef<HTMLInputElement>(null)
+
     useEffect(()=>{
         mRefMenu.current!.RefreshSize()
     },[])
+    
+    const styleHeader={
+        color:"wheat",border:"3px solid wheat",padding:20
+    }
+
+    function getHeader():ReactElement{
+        return(
+            <div style={styleHeader}>
+                <button onClick={()=>{
+                    mRefMenu.current!.Expand()
+                }}>Expand</button>
+                <button onClick={()=>{
+                    mRefMenu.current!.Collapse()
+                }}>Collapse</button>
+                <button onClick={()=>{
+                    if(!mRefInput.current?.value){
+                        alert('id for click - empty')
+                        return;
+                    }
+                    mRefMenu.current!.OpenMenuItemAndClick(mRefInput.current!.value)
+                }}>Click by Id</button>
+                <br/><br/>
+                <input ref={mRefInput} type={'text'} style={{width:270}}/>
+            </div>
+        )
+    }
 
     return(
         <div style={{display:"flex",width:"100%",height:"100%"}}>
@@ -30,41 +174,29 @@ export default function P7_1(){
                 <LeftMenu
                     style={{height:"100%"}}
                     ref={mRefMenu}
-                    // header={header(() => {
-                    //     return mRefMenu.current
-                    // })}
-                    header={<div style={{height:100}}></div>}
+                    header={getHeader()}
                     iconTree={<TbPointFilled size={12}/>}
                     paddingItem={15}
                     minWidth={55}
                     width={350}
-                    items={ListItem}
+                    items={GetItems()}
                     iconClose={<FaCaretRight color={"#6c6b6b"}/>}
                     iconOpen={<FaCaretDown/>}
                     iconToggle={<RiMenuFill size={30} color={"yellow"}/>}
                     footer={<div style={{height: 40, borderTop: "1px solid #6c6b6b"}}></div>}
+                    onClickMenuItem={(a,o)=>{
+                       mRefInput.current!.value= o.items!.id
+                    }}
+                    onClickToggle={(state)=>{
+                        if(state){//open
+                            if(mRefInput.current!.value){
+                                mRefMenu.current!.OpenMenuItemAndClick(mRefInput.current!.value)
+                            }
+                        }
+                    }}
 
-                    // onClickMenuItem={() => {
-                    //     storage.isManualClick = true;
-                    // }
-                    //}
-                    // onClickToggle={(state) => {
-                    //     if(state){
-                    //
-                    //         let myUrl = new URL(document.URL.replace(/#/g, "?"));
-                    //         const tag = myUrl.searchParams.get("page");
-                    //         if(tag){
-                    //             mRefMenu.current!.OpenMenuItemOnly(tag)
-                    //         }
-                    //     }
-                    // }
-                    //}
-
-                />
-
-
-
-            <div style={{height:"100%",width:"fit-content",background:"green"}}></div>
+                />`}/>
+            </div>
         </div>
     )
 }
